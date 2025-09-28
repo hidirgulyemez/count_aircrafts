60465d826939cbc0708e503a97578afe18fd06ef
hidirgulyemez <hidirgulyemez@gmail.com>
2025-09-28 21:39:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ def json_view():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8080")), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
